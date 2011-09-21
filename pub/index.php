6195f3e014a30ee1ef4b89db6a7b8cfe43d9a93e
David Rogers <david@ethos-development.com>
2011-09-21 00:02:07 -0400

    Upon further reflection, this is more or less how I'd like to use Gaelic\App::init()...

--- pub/index.php
+++ pub/index.php
@@ -2,11 +2,18 @@
 
 require_once '../lib/Gaelic/App.php';
 
+define_default( 'ROOT_PATH', realpath(dirname(dirname(__FILE__))) );
+
 \Gaelic\App::init(array(
-    'ROOT_PATH' => realpath(dirname(dirname(__FILE__))),
     'include_path' => array( 'app', 'lib' ),
     'routes' => array(
         array('/', '\HelloWorldPage'),
+        array('/HelloWorld', function( $request ){
+            return 'Hello World!';
+        }),
+        array('/Hello/:name', '\HelloNamePage', array(
+            ':name' => 'World'
+        )),
     ),
 ))->run();
 
