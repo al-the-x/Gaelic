98bd65b6ad4d66c43d78413efa6dff38f845a8fb
David Rogers <david@ethos-development.com>
2011-10-03 22:13:55 -0400

    That should work better...

--- etc/watchr.rb
+++ etc/watchr.rb
@@ -5,7 +5,7 @@ watch(/(lib|app)\/.+\.php$/) do |matches|
 
     testfile.sub!(/\.php/, 'Test.php') unless testfile.match(/Test\.php/)
 
-    testfile.sub('Gaelic', 'Test')
+    testfile.sub!('Gaelic', 'Test')
 
     system "clear && echo $(date): #{changed} && phpunit --verbose --configuration etc/phpunit.xml #{testfile}"
 end
